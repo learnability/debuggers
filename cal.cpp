69cf778ab25282c0c4e93faf352abc281eb1fd55
ashlamp08 <ashok.dhungana08@gmail.com>
2014-02-15 14:13:47 +0530

    cal.cpp changed

--- cal.cpp
+++ cal.cpp
@@ -1,3 +1,7 @@
+#include<iostream>
+using namespace std;
+
+
 //function to add two double precision numbers
 
 double add(double a, double b)
@@ -5,8 +9,28 @@ double add(double a, double b)
 	return a+b;	//return the sum of the given numbers
 }
 
-#include<iostream>
-using namespace std;
+double div(double a,double b) //function  for division between two numbers
+{  
+   double d;						
+      d=a/b;
+      return d;				//This returns the final answer after division
+}
+
+double mul( double x, double y) //function for multiplicaation of two numbers
+{
+	double m;
+	m=x*y;          //multiplying and storing in a separate variable
+	return m;       //returning the product
+}
+
+//function to return the difference between two numbers.
+double sub(double a, double b)
+{
+	return a-b; 	//returning the difference
+}
+
+
+//MAIN FUNCTION
 int main()
 {
 char c;
@@ -24,13 +48,13 @@ if( c=='1'||c=='2'||c=='3'||c=='4' )
 switch(c)
 {
 case '1':	cout<<"Ans="<<add(a,b);break;
-/*case '2':	cout<<"Ans="<<sub(a,b);break;
+case '2':	cout<<"Ans="<<sub(a,b);break;
 case '3':	cout<<"Ans="<<mul(a,b);break;
 case '4':	if(b)
 			cout<<"Ans="<<div(a,b);
 		else	
 			cout<<"Division by Zero not possible";
-		break;*/
+		break;
 case '5':	break;
 default : 	cout<<"Wrong Choice!! Enter Again";
 }
