d45054d0954ce8670b49207b1df2f651db43c580
Guest <guest@nsl37.(none)>
2014-02-15 13:56:25 +0530

    added add() to main

--- cal.cpp
+++ cal.cpp
@@ -1,3 +1,10 @@
+//function to add two double precision numbers
+
+double add(double a, double b)
+{
+	return a+b;	//return the sum of the given numbers
+}
+
 #include<iostream>
 using namespace std;
 int main()
@@ -6,24 +13,24 @@ char c;
 double a,b;
 do
 {
-cout<<"MENU\n1.Add 2 Numbers\n2.Subtract 2 Numbers\n3.Multiply 2 Numbers\n4.Divide 2 Numbers\n5.Exit\n";
-cout<<"Enter your choice\n";
+cout<<"\nMENU\n1.Add 2 Numbers\n2.Subtract 2 Numbers\n3.Multiply 2 Numbers\n4.Divide 2 Numbers\n5.Exit\n";
+cout<<"Enter your choice:\n";
 cin>>c;
 if( c=='1'||c=='2'||c=='3'||c=='4' )
 	{
-	cout<<"Enter Two Numbers";
+	cout<<"Enter Two Numbers:\n";
 	cin>>a>>b;
 	}
 switch(c)
 {
 case '1':	cout<<"Ans="<<add(a,b);break;
-case '2':	cout<<"Ans="<<sub(a,b);break;
+/*case '2':	cout<<"Ans="<<sub(a,b);break;
 case '3':	cout<<"Ans="<<mul(a,b);break;
 case '4':	if(b)
 			cout<<"Ans="<<div(a,b);
 		else	
 			cout<<"Division by Zero not possible";
-		break;
+		break;*/
 case '5':	break;
 default : 	cout<<"Wrong Choice!! Enter Again";
 }
